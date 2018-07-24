b575af94556a21cb1a0ac153c0e701e8988c764f
Nazik <mobidik460@gmail.com>
2018-07-24 23:59:37 +0300

    nov

--- script.js
+++ script.js
@@ -1,74 +1,78 @@
 
 
-var objects = [
- {value: 'dsfasd1', completed: false},
- {value: 'dsfasd2', completed: true},
- {value: 'dsfasd3', completed: true},
- {value: 'dsfasd4', completed: false},
- {value: 'dsfasd5', completed: false},
- {value: 'dsfasd5', completed: false},
+var businessCase = [
+  {value: 'dsfasd1', completed: false},
+  {value: 'dsfasd2', completed: true},
+  {value: 'dsfasd3', completed: true},
+  {value: 'dsfasd4', completed: false},
+  {value: 'dsfasd5', completed: false},
+  {value: 'dsfasd5', completed: false},
 ]
 
-var parent =  document.getElementById('parent');
+var parentUl =  document.getElementById('parent');
 
-function arri(num) {
- for (var i = 0; i < num.length;i++) {
+function innerBusinessCase(caselist) {
+  removeElements()
+
+  for (var i = 0; i < caselist.length;i++) {
   var li = document.createElement('li');
-  li.innerHTML = num[i]['value'];
-  parent.appendChild(li);
+  li.innerHTML = caselist[i]['value'];
+  parentUl.appendChild(li);
+  li.classList.add('elements');
 
-  if (num[i]['completed'] == true ) {
+  if (caselist[i]['completed'] == true ) {
   li.style.textDecoration = 'line-through';
-  li.classList.add('true');
   }
- }
-}
+  }
+  }
   
-
-
-
-  var val = document.getElementById('fix');
-  val.onkeypress = go;
-
+var addBusines = document.getElementById('input');
+addBusines.onkeypress = addBusinessCase;
 	
-	
-function go(event) {
+function addBusinessCase(event) {
   var code = event.keyCode;
-
   if (code == 13){
-  var li =  document.createElement('li');
-  li.classList.add('true');
-  li = this.value;
-  objects.push({value: li,completed: true})
-  val.value = '';
-  arri(objects)
-  }
   
+  businessCase.push({value: this.value,completed: true})
+  addBusines.value = '';
+  innerBusinessCase(businessCase)
+  }
+  }
+ 
+function removeElements(){
+  var del = document.querySelectorAll('.elements');
+  for (var i = 0;i < del.length;i++){
+  parentUl.removeChild(del[i]);
+  }
+  }
 
 
 
 
-   parent.appendChild(li);   
 
-  var elem = document.getElementsByClassName('www');
-  for (var i = 0;i < elem.length;i++) {
-  elem[i].addEventListener('click', func);}
-}
 
-alert(objects)
-function func(){
-  this.style.color = 'red'
-  this.classList.add('ppp')
-}
-  var rem = document.getElementById('rem');
-  rem.addEventListener('click', deli);
 
-function deli(){
-	var del = document.querySelectorAll('.www.ppp');
-for (var i = 0;i < del.length;i++){
-	this.parentElement.removeChild(del[i]);
-}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 var use = document.getElementById('user');
@@ -80,23 +84,19 @@ use.addEventListener('click', addText);
 function addText(){
 	var text = prompt("name","")
 	event.target.innerHTML = text;
-	var tr = document.createElement('tr');
-	tr.classList.add('www')
-	use.appendChild(tr);
-	var tr = document.getElementsByClassName('www');
-	for (var i = 0;i<3;i++){
+for(var i = 0;i < use.length;i++){
+	var tr = document.createElement('tr')
+	for(var j = 0;j < 2;j++){
 		var td = document.createElement('td');
-		tr[i].appendChild(td);
+		tr.appendChild(td)
 	}
+	var name = = document.getElementById('');
+	table.appendChild(tr);
+}
+	
 	
 
 }
 
-var button = document.getElementById('createLi');
-button.addEventListener('click', creatElem);
-function creatElem(){
-		var li = document.createElement('li');
-	li.innerHTML = 'новая li';
-	ul.appendChild(li);
-}
+
 
